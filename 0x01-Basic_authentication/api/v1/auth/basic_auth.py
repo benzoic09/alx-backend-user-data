302dfedc93efee2c6cf56a7de2154a7ebcb66ed3
Ben Kimani <benzoic09@gmail.com>
2024-06-04 21:51:20 +0000

    task 11

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -49,7 +49,7 @@ class BasicAuth(Auth):
 
     def user_object_from_credentials(
             self, user_email: str, user_pwd: str) -> TypeVar('User'):
-        """ Returns the User instance based on his email and password"""
+        """ Returns the User instance based on his email and password """
         if user_email is None or not isinstance(user_email, str):
             return None
         if user_pwd is None or not isinstance(user_pwd, str):
