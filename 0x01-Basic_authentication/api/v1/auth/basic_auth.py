d2fc9e769ae5b86e028aa05c05e2d32939f7eeaf
Ben Kimani <benzoic09@gmail.com>
2024-06-04 19:17:39 +0000

    task 6

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-""" BasicAuth module
-"""
+""" BasicAuth module"""
 from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
-    """ BasicAuth class that inherits from Auth
+    """
+    BasicAuth class that inherits from Auth
     """
     pass
