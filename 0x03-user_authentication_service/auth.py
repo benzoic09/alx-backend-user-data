9e608970d99afdf03fddee0af8fc4b54b03142c5
Ben Kim <benzoic09@gmail.com>
2024-06-11 20:47:27 +0300

    valid_login

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -21,18 +21,28 @@ def _hash_password(password: str) -> bytes:
 
 
 class Auth:
-    """Auth class to interact with the authentication database.
-    """
+    """Auth class to interact with the authentication database."""
 
     def __init__(self):
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
         """Register a new user with the given email and password."""
+
         try:
             self._db.find_user_by(email=email)
-            raise ValueError("user {} already exists".format(email))
+            raise ValueError(f"user {email} already exists")
         except NoResultFound:
             hashed_password = _hash_password(password)
             new_user = self._db.add_user(email, hashed_password)
             return new_user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validate login credentials."""
+        try:
+            user = self._db.find_user_by(email=email)
+            if bcrypt.checkpw(password.encode('utf-8'), user.hashed_password):
+                return True
+            return False
+        except NoResultFound:
+            return False
